b3684942e8b6a8f2e3b80ee200543cddbadfaa3e
huyanhdo <86727493+huyanhdo@users.noreply.github.com>
2022-05-13 00:30:26 +0700

    Update LAST.py

--- LAST.py
+++ LAST.py
@@ -1,23 +1,10 @@
 import os  
 import math 
+import networkx as nx
+import matplotlib.pyplot as plt
 # CONSTANT
 INF = 9999999999
-# NUM_NODES = 8
-# graph = [[] for i in range(8)]
-# graph[0] = [1,2]
-# graph[1] = [0,3,4]
-# graph[2] = [0,4,5]
-# graph[3] = [1,4]
-# graph[4] = [3,5,6,7]
-# graph[5] = [2,4]
-# graph[6] = [4]
-# graph[7] = [4]
-# report_size = [0,0,0,1,0,1,1,1]
-# num_relay = report_size.count(0)
-# if report_size[0] ==0:
-# 	num_relay+=1
 
-# Import data
 
 def inputToGraph(path):		
 	f = open(path,'r')
@@ -52,6 +39,7 @@ def inputToGraph(path):
 	num_relay = report_size.count(0)
 	if report_size[0] ==0:
 		num_relay+=1
+
 	return graph,report_size,Q,H,R,NUM_NODE,num_edge,num_relay
 
 # Create graph_hat: a completer graph with out relay node which each edge's weight equals to the min distant from u to v in original graph
@@ -89,11 +77,11 @@ def create_graph_hat(graph,report_size,NUM_NODE):
 		if(report_size[u] !=0 and report_size[v]!=0) or (u ==0 and report_size[v]!=0) or (v == 0 and report_size[u]!=0):
 			graph_hat[u].append(v)
 		else:
-			del all_pair_shortest_path[(u,v)]		
+			del all_pair_shortest_path[(u,v)]	
 	return graph_hat , all_pair_shortest_path
 
-# Find (2,3)-LAST tree from G_hat 
-# (2,3) light approximate shortest-path tree
+# Find (3,2)-LAST tree from G_hat 
+# (3,2) light approximate shortest-path tree
 def LAST(graph,edge,report_size,num_relay,NUM_NODE):
 	def find(parent,i):
 		if parent[i] == i:
@@ -118,7 +106,6 @@ def LAST(graph,edge,report_size,num_relay,NUM_NODE):
 		parent = [i for i in range(NUM_NODE)]
 		rank = [0 for i in range(NUM_NODE)]
 		edge = dict(sorted(edge.items(),key = lambda item : item[1]))
-		
 		number_of_edge = NUM_NODE*2 - 2
 		for node,weight in edge.items():  
 			x = find(parent,node[0])
@@ -162,27 +149,28 @@ def LAST(graph,edge,report_size,num_relay,NUM_NODE):
 			if report_size[i]!=0:
 				new_edge[(i,parent[i])] = edge[(parent[i],i)]
 				new_edge[(parent[i],i)] = edge[(parent[i],i)]
-
 		return new_edge,parent,dist_ts
 				
 	# FIND LAST
 	def LAST_TREE():
 		Tm = [[] for i in range(NUM_NODE)]
-
-		for u,v in kruskal(edge).keys():
+		w = kruskal(edge)
+		
+		for u,v in w.keys():
 			Tm[u].append(v)
 
 		for i in Tm:
 			i.sort()
- 
-		Ts,parent_Ts,dis_Ts = SPT_with_weight(graph,0,edge,0) 
+			
+		Ts,parent_Ts,dis_Ts = SPT_with_weight(graph,0,edge,0)
 
-		alpha = 2
+
+		alpha = 3
 		def initialize():
 			dis = [INF for i in range(NUM_NODE)]
 			dis[0] = 0
 			parent = [-1 for i in range(NUM_NODE)]
-			parent[0] = -1
+			# parent[0] = -1
 			check = [False for i in range(NUM_NODE)]
 			return dis,parent,check
 
@@ -190,6 +178,7 @@ def LAST(graph,edge,report_size,num_relay,NUM_NODE):
 			if dis[v] > dis[u] + edge[(u,v)]:
 				dis[v] = dis[u] + edge[(u,v)]
 				parent[v] = u
+
 			return dis,parent 
 
 		def add_path(dis,parent,node):
@@ -200,28 +189,31 @@ def LAST(graph,edge,report_size,num_relay,NUM_NODE):
 
 		def dfs(node,dis,parent,check):
 			check[node] = True
-			if(dis[node]>alpha*dis_Ts[node]):
+			if(dis[node]>=alpha*dis_Ts[node]):
 				dis,parent = add_path(dis,parent,node)	
 			for v in Tm[node]:
 				if check[v] == False:
 					dis,parent = relax(node,v,dis,parent)
 					dis,parent = dfs(v,dis,parent,check)
 					dis,parent = relax(v,node,dis,parent)
-
 			return dis,parent
+
 		dis,parent,check = initialize()
+
 		dis,parent = dfs(0,dis,parent,check)
+
 		tree = [[] for i in range(NUM_NODE)]
+
 		for i in range(1,NUM_NODE):
 			if report_size[i]!=0:
 				tree[i].append(parent[i])
 				tree[parent[i]].append(i)
-			# tree[parent[i]].append(i)
+			
+		# print(tree)
 		return tree
 	return LAST_TREE()	
 
 def create_graph_hat_hat(graph,last_tree,report_size,NUM_NODE):
-	
 	def find_path(graph,adjacent_list,node,graph_hat_hat,graph_hat_hat_edge):
 		queue = []
 		check = [False for i in range(NUM_NODE)]
@@ -238,35 +230,32 @@ def create_graph_hat_hat(graph,last_tree,report_size,NUM_NODE):
 					check[i] = True
 					father[i] = current
 					queue.append(i)
-			for i in adjacent_list:
-				if check[i] == False:
-					if_break = False
-					break
-			if if_break:
-				break 
+			
 
 		for it in adjacent_list:
 			item = it
-			while True:
-				if father[item] != -1:		
-	 				if (father[item],item) not in graph_hat_hat_edge:
-	 					graph_hat_hat_edge.append((father[item],item))
-	 					graph_hat_hat_edge.append((item,father[item]))
-	 					graph_hat_hat[item].append(father[item])
-	 					graph_hat_hat[father[item]].append(item)
-	 					item = father[item]
-	 				else:
-	 					break
-				else:
-	 				break
+			while father[item] != -1:		
+ 				if (father[item],item) not in graph_hat_hat_edge:
+ 					graph_hat_hat_edge.append((father[item],item))
+ 					graph_hat_hat_edge.append((item,father[item]))
+
+ 				if father[item] not in graph_hat_hat[item]:
+ 					graph_hat_hat[item].append(father[item])
+
+ 				if item not in graph_hat_hat[father[item]]:
+ 					graph_hat_hat[father[item]].append(item)
+
+ 				item = father[item]
+				
 		return graph_hat_hat,graph_hat_hat_edge
 
 	graph_hat_hat = [ [] for i in range(NUM_NODE)]
 	graph_hat_hat_edge = []
-
 	for index,adjacent_list in enumerate(last_tree):
 		if len(adjacent_list)>0:
 			graph_hat_hat,graph_hat_hat_edge = find_path(graph,adjacent_list,index,graph_hat_hat,graph_hat_hat_edge)
+	# print(graph_hat_hat_edge)
+
 	return graph_hat_hat 
 
 def SPT(NUM_NODE,graph):
@@ -320,12 +309,22 @@ def energy_cost(num_node,tree,t,r,q,report_size,level):
 	return res*(t+r)
 
 
-mecatDataPath = 'C:/Users/huyan/OneDrive/Desktop/data for lstm/dataset4mecat/mecat_rn/' 
+# mecatDataPath = 'C:/Users/huyan/OneDrive/Desktop/data for lstm/dataset4mecat/mecat_rn/' 
+mecatDataPath = 'C:/Users/huyan/OneDrive/Desktop/test/data/rn_'
+# mecatDataFiles = os.listdir(mecatDataPath)
 
-mecatDataFiles = os.listdir(mecatDataPath)
+# mecatDataFiles = sorted(mecatDataFiles,reverse=True)
 
-mecatDataFiles = sorted(mecatDataFiles,reverse=False)
-
+mecatDataFiles = ['l100_4_10.test','l110_4_10.test','l120_4_10.test','l130_4_10.test','l140_4_10.test','l150_4_10.test'
+        ,'l160_4_10.test','l170_4_10.test','l180_4_10.test','l190_4_10.test','l100_4_15.test','l110_4_15.test'
+        ,'l120_4_15.test','l130_4_15.test','l140_4_15.test','l150_4_15.test','l160_4_15.test','l170_4_15.test'
+        ,'l180_4_15.test','l190_4_15.test','l100_4_20.test','l110_4_20.test','l120_4_20.test','l130_4_20.test'
+        ,'l140_4_20.test','l150_4_20.test','l160_4_20.test','l170_4_20.test','l180_4_20.test','l190_4_20.test'
+        ,'m100_4_10.test','m110_4_10.test','m120_4_10.test','m130_4_10.test','m140_4_10.test','m150_4_10.test'
+        ,'m160_4_10.test','m170_4_10.test','m180_4_10.test','m190_4_10.test','m100_4_15.test','m110_4_15.test'
+        ,'m120_4_15.test','m130_4_15.test','m140_4_15.test','m150_4_15.test','m160_4_15.test','m170_4_15.test'
+        ,'m180_4_15.test','m190_4_15.test','m100_4_20.test','m110_4_20.test','m120_4_20.test','m130_4_20.test'
+        ,'m140_4_20.test','m150_4_20.test','m160_4_20.test','m170_4_20.test','m180_4_20.test','m190_4_20.test']
 for i in mecatDataFiles:
 	graph,report_size,Q,T,R,NUM_NODE,num_edge,num_relay = inputToGraph(mecatDataPath + i)
 	graph_hat,all_pair_shortest_path = create_graph_hat(graph,report_size,NUM_NODE)
@@ -335,10 +334,3 @@ for i in mecatDataFiles:
 	lev = level(tree)
 	print(i,energy_cost(NUM_NODE,tree,T,R,Q,report_size,lev))
 
-
-	# tree1 = SPT(NUM_NODE,graph)
-	# lev1 = level(tree1)
-	# print(i,energy_cost(NUM_NODE,tree1,T,R,Q,report_size,lev1)) 
-	print()
-# graph,report_size,Q,H,R,NUM_NODE,num_edge = inputToGraph(mecatDataPath + mecatDataFiles[0])
-# print(mecatDataFiles[0],graph)
